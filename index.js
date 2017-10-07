605947ca56bacd14ba6db6ab4f8d17c379cdddf8
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-10-07 12:36:49 +0200

    mods

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log('first commit');
+const passport = require('passport');
+const SamlStrategy = require('passport-saml').Strategy;
+
+passport.use(new SamlStrategy(
+  {
+    path: '/login/callback',
+    entryPoint: 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+    issuer: 'passport-saml'
+  },
+  (profile, done) => {  })
+);
 
 module.exports = {};
